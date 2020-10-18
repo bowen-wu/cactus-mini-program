6e4ff2f3e0e4ccefb475c88765b3c42210c3d9be
bowen-wu <13793248162@163.com>
2020-10-18 21:56:47 +0800

    feat: complete request functional

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,7 @@ const config = {
     NODE_ENV: '"development"'
   },
   defineConstants: {
-    BASE_URL: JSON.stringify('http://182.92.63.233:8888'),
+    BASE_URL: JSON.stringify('https://api.imcactus.com'),
     BASE_PREFIX: JSON.stringify('/api')
   },
   mini: {},
